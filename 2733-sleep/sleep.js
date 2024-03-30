e31c2eb2a77236a4413bd0bb6adaa2e1f580b346
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-03-30 18:53:46 +0530

    Time: 55 ms (40.49%) | Memory: 48.9 MB (39.24%) - LeetSync

---
+++ 2733-sleep/sleep.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+
+    return new Promise((res,rej)=> {
+        setTimeout(()=> {
+            res('Hello')
+    },millis)
+        
+    })
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
