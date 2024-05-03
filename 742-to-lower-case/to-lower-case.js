da09866a07db0d9cfe490f3fdeab047cf11bb0b6
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-03 17:37:48 +0530

    Time: 60 ms (12.50%) | Memory: 48.3 MB (92.77%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,14 +3,5 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-     let result = '';
-    for (let i = 0; i < s.length; i++) {
-        const ascii = s.charCodeAt(i);
-        if (ascii >= 65 && ascii <= 90) {
-            result += String.fromCharCode(ascii + 32);
-        } else {
-            result += s.charAt(i);
-        }
-    }
-    return result;
+    return s.toLowerCase()
 };
