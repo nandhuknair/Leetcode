ad04b9780a5da418677827c17e6d0fb8cb266b36
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-03 11:03:10 +0530

    Time: 79 ms (33.91%) | Memory: 51.8 MB (43.48%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -3,13 +3,7 @@
  * @return {number}
  */
 var findMaxK = function(nums) {
-    // let maxK = -1
-    // for(let num of nums){
-    //     if(nums.includes(-num)){
-    //         maxK = Math.max(maxK,Math.abs(num))
-    //     }
-    // }
-    // return maxK
+
 
     let maxK = -1
     let numSet = new Set (nums)
