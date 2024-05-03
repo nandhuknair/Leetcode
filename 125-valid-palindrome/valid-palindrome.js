7bacbb94fcfd928099f214213720746e052a1b67
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-03 17:25:10 +0530

    Time: 53 ms (92.67%) | Memory: 53.9 MB (32.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,25 +4,9 @@
  */
  
 function isPalindrome(s) {
-    // Helper function to remove non-alphanumeric characters and convert to lowercase
-    function cleanString(str) {
-        return str.replace(/[^a-z0-9]/ig, '').toLowerCase();
-    }
+   
+    const cleanString = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    
 
-    // Helper function to check if a string is a palindrome
-    function checkPalindrome(str) {
-        if (str.length <= 1) {
-            return true;
-        }
-        if (str[0] === str[str.length - 1]) {
-            return checkPalindrome(str.slice(1, -1));
-        } else {
-            return false;
-        }
-    }
-
-    // Main function logic
-    const cleanedString = cleanString(s);
-    return checkPalindrome(cleanedString);
+    return cleanString === cleanString.split("").reverse().join("");
 }
-
