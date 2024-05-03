f525f7f57252e5d59e4418f119d57a0a33e3cbf5
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-03 16:21:24 +0530

    Time: 56 ms (93.92%) | Memory: 51.8 MB (62.13%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,10 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let arr = []
-    for(let i = 0 ; i < nums.length; i++){
-        arr.push(nums[i])
-    }
-
-    return arr.concat(nums)
+   return nums.concat(nums)
 };
