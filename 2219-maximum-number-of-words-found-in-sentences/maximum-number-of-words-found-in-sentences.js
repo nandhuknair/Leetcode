cd2dbdc64255f35c9c3bbb0f7415d4529f34f6f2
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-05-03 16:43:10 +0530

    Time: 66 ms (12.28%) | Memory: 51 MB (69.97%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -6,8 +6,7 @@ var mostWordsFound = function(s) {
     let count = 0 
     
     for(let i = 0 ; i < s.length ; i++){
-        let words = s[i].split(' ')
-        let wordsCount = words.length
+        let wordsCount = s[i].split(' ').length
         if(wordsCount > count){
             count = wordsCount
         }
