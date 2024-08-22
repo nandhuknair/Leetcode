17148e6f25beb8cdff707fcf3dd1af8411bd0e2d
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-08-22 12:28:43 +0530

    Time: 46 ms (85.41%) | Memory: 48.2 MB (96.69%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,13 +3,17 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let goodPair = 0 
-    for(let i = 0 ; i < nums.length ; i++){
-        for(let j = i+1 ; j < nums.length ; j++){
-            if(nums[i] === nums[j]){
-                 goodPair ++
-            }
+ 
+    let count =  0;
+    const map = new Map()
+    for( const num of nums){
+        if(map.has(num)){
+            count += map.get(num)
+            map.set(num,map.get(num)+1)
+        }
+        else {
+            map.set(num,1)
         }
     }
-    return goodPair
+    return count 
 };
