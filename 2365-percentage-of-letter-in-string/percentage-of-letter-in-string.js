828ee2bf2dd3e830dfb1d68a476845c58e448902
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-12-13 11:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (35.23%) - LeetSync

---
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+let count = 0 
+
+for (let char of s ){
+    if(char === letter){
+        count++
+    }
+}
+return Math.floor((count / s.length )*100)
+
+};
